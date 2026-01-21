8f8be8af1064383748b20c867ff7c8b94634aa84
Test User <test@example.com>
2026-01-21 08:05:47 +0500

    разработа FormMenu

---
+++ ShoesProject/FormMenu.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace ShoesProject
+{
+    partial class FormMenu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            DataGridViewCellStyle dataGridViewCellStyle1 = new DataGridViewCellStyle();
+            lblUserName = new Label();
+            btnLogut = new Button();
+            panelTop = new Panel();
+            dgvButtons = new DataGridView();
+            btnOrders = new Button();
+            btnProducts = new Button();
+            panelTop.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dgvButtons).BeginInit();
+            SuspendLayout();
+            // 
+            // lblUserName
+            // 
+            lblUserName.AutoSize = true;
+            lblUserName.Dock = DockStyle.Right;
+            lblUserName.Location = new Point(189, 0);
+            lblUserName.Name = "lblUserName";
+            lblUserName.Size = new Size(45, 19);
+            lblUserName.TabIndex = 6;
+            lblUserName.Text = "label1";
+            lblUserName.TextAlign = ContentAlignment.MiddleRight;
+            // 
+            // btnLogut
+            // 
+            btnLogut.BackColor = Color.MediumSpringGreen;
+            btnLogut.Dock = DockStyle.Right;
+            btnLogut.FlatAppearance.BorderSize = 0;
+            btnLogut.FlatStyle = FlatStyle.Flat;
+            btnLogut.Location = new Point(234, 0);
+            btnLogut.Name = "btnLogut";
+            btnLogut.Size = new Size(150, 30);
+            btnLogut.TabIndex = 5;
+            btnLogut.Text = "Выход";
+            btnLogut.UseVisualStyleBackColor = false;
+            // 
+            // panelTop
+            // 
+            panelTop.BackColor = Color.White;
+            panelTop.Controls.Add(lblUserName);
+            panelTop.Controls.Add(btnLogut);
+            panelTop.Dock = DockStyle.Top;
+            panelTop.Location = new Point(0, 0);
+            panelTop.Name = "panelTop";
+            panelTop.Padding = new Padding(0, 0, 0, 10);
+            panelTop.Size = new Size(384, 40);
+            panelTop.TabIndex = 1;
+            // 
+            // dgvButtons
+            // 
+            dgvButtons.AllowUserToAddRows = false;
+            dgvButtons.AllowUserToDeleteRows = false;
+            dgvButtons.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dgvButtons.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
+            dgvButtons.BackgroundColor = Color.White;
+            dgvButtons.BorderStyle = BorderStyle.None;
+            dgvButtons.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvButtons.ColumnHeadersVisible = false;
+            dataGridViewCellStyle1.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = SystemColors.Window;
+            dataGridViewCellStyle1.Font = new Font("Times New Roman", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            dataGridViewCellStyle1.ForeColor = SystemColors.ControlText;
+            dataGridViewCellStyle1.SelectionBackColor = SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = DataGridViewTriState.True;
+            dgvButtons.DefaultCellStyle = dataGridViewCellStyle1;
+            dgvButtons.Location = new Point(0, 40);
+            dgvButtons.MultiSelect = false;
+            dgvButtons.Name = "dgvButtons";
+            dgvButtons.ReadOnly = true;
+            dgvButtons.RowHeadersVisible = false;
+            dgvButtons.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dgvButtons.Size = new Size(384, 321);
+            dgvButtons.TabIndex = 2;
+            // 
+            // btnOrders
+            // 
+            btnOrders.Anchor = AnchorStyles.None;
+            btnOrders.Location = new Point(118, 139);
+            btnOrders.Name = "btnOrders";
+            btnOrders.Size = new Size(150, 30);
+            btnOrders.TabIndex = 3;
+            btnOrders.Text = "Заказы";
+            btnOrders.UseVisualStyleBackColor = true;
+            // 
+            // btnProducts
+            // 
+            btnProducts.Anchor = AnchorStyles.None;
+            btnProducts.Location = new Point(118, 194);
+            btnProducts.Name = "btnProducts";
+            btnProducts.Size = new Size(150, 30);
+            btnProducts.TabIndex = 4;
+            btnProducts.Text = "Товары";
+            btnProducts.UseVisualStyleBackColor = true;
+            // 
+            // FormMenu
+            // 
+            AutoScaleDimensions = new SizeF(9F, 19F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(384, 361);
+            Controls.Add(btnProducts);
+            Controls.Add(btnOrders);
+            Controls.Add(dgvButtons);
+            Controls.Add(panelTop);
+            Font = new Font("Times New Roman", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            Margin = new Padding(4);
+            Name = "FormMenu";
+            Text = "Меню";
+            panelTop.ResumeLayout(false);
+            panelTop.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)dgvButtons).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Label lblUserName;
+        private Button btnLogut;
+        private Panel panelTop;
+        private DataGridView dgvButtons;
+        private Button btnOrders;
+        private Button btnProducts;
+    }
+}
---
+++ ShoesProject/FormMenu.cs
@@ -0,0 +1,22 @@
+﻿using ShoesProject.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ShoesProject
+{
+    public partial class FormMenu : Form
+    {
+        public User CurrentUser { get; private set; }
+        public bool IsGuest { get; private set; }
+
+        public FormMenu()
+        {
+            InitializeComponent();
+        }
+    }
+}
